4140c3973805235f68f6fa51dc1591f9c4b47584
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-06 17:36:08 +0800

    Fri Jan  6 05:36:08 PM CST 2023

--- app/archive-tool-7z.js
+++ app/archive-tool-7z.js
@@ -20,7 +20,7 @@ let main = async function () {
 
     let cmd
     if (fs.lstatSync(file).isDirectory()) {
-      cmd = `cd "${file}"; 7z a -t7z "${path.resolve(dirname, filename + '.7z')}" -mx9 -aoa -ms=on -m0=lzma2 -cu=on *`
+      cmd = `cd "${file}"; 7z a -t7z "${path.resolve(dirname, filename + '.7z')}" -mx9 -aoa -ms=on -m0=lzma2 -mmt=off -mcu=on *`
     }
     else {
       let ext
