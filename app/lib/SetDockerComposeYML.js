4407e82354dcee3ae754082a3586f980d85b9101
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-21 22:57:58 +0800

    Sun May 21 10:57:58 PM CST 2023

--- app/lib/SetDockerComposeYML.js
+++ app/lib/SetDockerComposeYML.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const fs = require('fs')
 
-module.exports = function (file) {
+module.exports = function (file, isBig5 = false) {
   let filename = path.basename(file)
   let dirname = path.dirname(file)
 
-  let template = fs.readFileSync(path.join(__dirname, '../../docker-compose-template.yml'), 'utf8')
+  let templateFilename = 'docker-compose-template.yml'
+  if (isBig5) {
+    templateFilename = 'docker-compose-big5-template.yml'
+  }
+
+  let template = fs.readFileSync(path.join(__dirname, '../../' + templateFilename), 'utf8')
+  
   template = template.replace(/\[SOURCE\]/, `${dirname}`)
   // template = template.replace(/\[CMD\]/, `node /app/split-pdf.js "/input/${filename}"`)
   template = template.replace(/\[INPUT\]/, filename)
