e58a7a7961bba22d67b516aa5be4e1ff61bf7e9e
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-19 17:10:38 +0800

    西元2024年02月19日 (週一) 17時10分38秒 CST

--- app/archive-list.js
+++ app/archive-list.js
@@ -42,6 +42,7 @@ let main = async function () {
 
       // fs.copyFileSync(listFilePath, path.join(directoryPath, path.basename(listFilePath)));
       fs.renameSync(listFilePath, path.join(directoryPath, path.basename(listFilePath)));
+      listFilePath = path.join(directoryPath, path.basename(listFilePath))
       fs.renameSync(archiveFilePath, path.join(directoryPath, path.basename(archiveFilePath)));
 
       fs.rmdirSync(directoryPath + '.bak', { recursive: true });
