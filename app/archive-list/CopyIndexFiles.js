9d3284623d0511aa6d0c0ac2d74ca1d12fa37d4f
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-23 17:22:53 +0800

    西元2025年02月23日 (週日) 17時22分53秒 CST

---
+++ app/archive-list/CopyIndexFiles.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const ListFiles = require('../lib/ListFiles')
+
+const isIndexFile = function (file) {
+  return file.endsWith('.note.xlsx') || 
+    file.endsWith('.note.docx') ||
+    file.startsWith('cluster-windows')
+}
+
+const CopyIndexFiles = function (directoryPath, ENABLE_GDRIVE = false) {
+    let fileList = ListFiles(path.join(directoryPath + ".bak"))
+
+    for (let file of fileList) {
+      if (isIndexFile(file)) {
+        // console.log(noteFilePath, fs.existsSync(noteFilePath))
+        if (fs.existsSync(file)) {
+          if (ENABLE_GDRIVE) {
+            fs.copyFileSync(file, path.join(gdriveArchiveDir, path.basename(file)))
+          }
+          fs.renameSync(file, path.join(directoryPath, path.basename(file)))
+        }
+      }
+    }
+}
+
+module.exports = CopyIndexFiles
